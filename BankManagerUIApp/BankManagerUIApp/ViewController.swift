1e704752e0fb3439f5f861aa67f893e41386b37b
Dylan-Yoon <d.dylany21@gmail.com>
2022-11-09 16:42:47 +0900

    feat: timerLabel constrain 및 구현

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -16,9 +16,13 @@ class ViewController: UIViewController {
         
         view.addSubview(buttonStackView)
         addTopStackview(stackview: buttonStackView,headButton: customerAddButton, tailButton: resetButton)
+        
+        let timerLabel = timerLabel()
+        view.addSubview(timerLabel)
+        timerLableForTop(timerLabel: timerLabel, for: buttonStackView)
     }
     
-    func addTopStackview(stackview: UIStackView, headButton: UIButton, tailButton: UIButton) {
+    private func addTopStackview(stackview: UIStackView, headButton: UIButton, tailButton: UIButton) {
         stackview.addArrangedSubview(headButton)
         stackview.addArrangedSubview(tailButton)
         
@@ -29,7 +33,7 @@ class ViewController: UIViewController {
         headButton.widthAnchor.constraint(equalTo: tailButton.widthAnchor).isActive = true
     }
     
-    func taskButton(title: String, textColor: UIColor) -> UIButton {
+    private func taskButton(title: String, textColor: UIColor) -> UIButton {
         let button = UIButton()
         button.setTitle(title, for: .normal)
         button.setTitleColor(textColor, for: .normal)
@@ -40,7 +44,7 @@ class ViewController: UIViewController {
         return button
     }
     
-    func topStackView() -> UIStackView {
+    private func topStackView() -> UIStackView {
         let stackview = UIStackView()
         stackview.translatesAutoresizingMaskIntoConstraints = false
         stackview.axis = .horizontal
@@ -48,6 +52,22 @@ class ViewController: UIViewController {
         stackview.alignment = .center
         
         return stackview
+    }
+    
+    private func timerLabel() -> UILabel {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "업무시간 - " + "00:00:000"
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.textAlignment = .center
+        
+        return label
+    }
+    
+    private func timerLableForTop(timerLabel: UILabel,for topAnchor: UIView) {
+        timerLabel.topAnchor.constraint(equalTo: topAnchor.bottomAnchor, constant: 15).isActive = true
+        timerLabel.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        timerLabel.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
 }
 
