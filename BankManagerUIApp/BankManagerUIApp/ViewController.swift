6bda25410e4244cdc4b18980388051b31d5cf8a2
Dylan-Yoon <d.dylany21@gmail.com>
2022-11-09 16:28:43 +0900

    feat: 상단 버튼 구현

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -7,12 +7,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let buttonStackView = topStackView()
+        let customerAddButton = taskButton(title: "고객 10명 추가", textColor: .blue)
+        let resetButton = taskButton(title: "초기화", textColor: .red)
+        
+        view.addSubview(buttonStackView)
+        addTopStackview(stackview: buttonStackView,headButton: customerAddButton, tailButton: resetButton)
     }
+    
+    func addTopStackview(stackview: UIStackView, headButton: UIButton, tailButton: UIButton) {
+        stackview.addArrangedSubview(headButton)
+        stackview.addArrangedSubview(tailButton)
+        
+        stackview.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        stackview.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        stackview.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+        headButton.widthAnchor.constraint(equalTo: tailButton.widthAnchor).isActive = true
+    }
+    
+    func taskButton(title: String, textColor: UIColor) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(textColor, for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .caption1)
+        button.titleLabel?.textAlignment = .center
+        button.translatesAutoresizingMaskIntoConstraints = false
 
-
+        return button
+    }
+    
+    func topStackView() -> UIStackView {
+        let stackview = UIStackView()
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        stackview.axis = .horizontal
+        stackview.distribution = .fill
+        stackview.alignment = .center
+        
+        return stackview
+    }
 }
 
